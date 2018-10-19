5f4dcf10a3c0b3029996315793a3fa6fcfdd0f7a
wujingchun <wjc123456>
2018-10-19 15:18:52 +0800

    maven profiles

--- spring-boot-multi-datasource/src/main/java/com/example/boot/mapper/UserMapper.java
+++ spring-boot-multi-datasource/src/main/java/com/example/boot/mapper/UserMapper.java
@@ -1,8 +1,14 @@
 package com.example.boot.mapper;
 
 import com.example.boot.domain.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
     User findOne(long id);
+
+    List<User> findAll(@Param("table") String table, Map<String, Object> params, @Param("limit") int limit);
 }
